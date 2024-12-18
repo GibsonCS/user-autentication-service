c948b6869bbd7d755f69b8f6dce2c007fedfaaf8
Gibson Cruz <gibson8fla@gmail.com>
2024-12-18 13:34:08 -0300

    feat: implements jwt generation function

---
+++ src/util/jwtGenerate.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const KEY = process.env.JWT_SECRET_KEY
+
+export const generateJWT = (payload) => {
+    const options = {
+        expiresIn: '1h'
+    }
+    return jwt.sign(payload, KEY, options)
+}
