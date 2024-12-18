1236df4de13ec99c9f3b090bea05da1c2c39c051
Gibson Cruz <gibson8fla@gmail.com>
2024-12-18 14:14:17 -0300

    chore: returns false if user not exist

--- src/services/loginService.ts
+++ src/services/loginService.ts
@@ -5,9 +5,12 @@ import { generateJWT } from '../util/jwtGenerate.js'
 
 export const loginService = async (credentials: LoginInput) => {
     const user = await userRepository.getUserByLogin(credentials.username)
-    const passwordFromDB: string = user.password
-    const isTrue = await comparePassword(credentials.password, passwordFromDB);
-    if (isTrue) {
-        return generateJWT({ username: user.username })
+    if (user) {
+        const passwordFromDB: string = user.password
+        const isTrue = await comparePassword(credentials.password, passwordFromDB);
+        if (isTrue) {
+            return generateJWT({ username: user.username })
+        }
     }
+    return false
 }
