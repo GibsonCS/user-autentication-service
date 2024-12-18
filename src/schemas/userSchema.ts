14e387e003e6e0220248fbfef508495a62d0a066
Gibson Cruz <gibson8fla@gmail.com>
2024-12-18 13:39:08 -0300

    feat: creates login validation

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -6,4 +6,10 @@ export const userValidation = z.object({
     email: z.string().email()
 })
 
+export const loginValidation = z.object({
+    username: z.string(),
+    password: z.string()
+})
+
 export type UserInput = z.infer<typeof userValidation>
+export type LoginInput = z.infer<typeof loginValidation>
