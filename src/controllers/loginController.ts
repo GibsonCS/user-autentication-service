dfdfb7b1f6c4b5d243de4258b7602fcb514ec880
Gibson Cruz <gibson8fla@gmail.com>
2024-12-18 13:40:35 -0300

    feat: implements login controller

---
+++ src/controllers/loginController.ts
@@ -0,0 +1,23 @@
+import { FastifyRequest, FastifyReply } from "fastify"
+import { LoginInput, loginValidation } from "../schemas/userSchema"
+import * as loginService from '../services/loginService.js'
+
+export const loginController = async (req: FastifyRequest, reply: FastifyReply) => {
+    try {
+        const data: LoginInput = loginValidation.parse(req.body)
+        const jwt = await loginService.loginService(data)
+        if (jwt) {
+            reply.setCookie('authToken', jwt, {
+                httpOnly: true,
+                secure: false,
+                path: '/',
+                maxAge: 3600
+            }).status(200)
+        } else {
+            reply.status(401).send({ message: 'Verifique as credencias.' })
+        }
+    } catch (err) {
+        console.error(err)
+        throw new Error(err)
+    }
+}
