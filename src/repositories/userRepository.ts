d5dd5117331b06b54eb02181873d0648b4cbe060
Gibson Cruz <gibson8fla@gmail.com>
2024-12-18 13:43:47 -0300

    feat: implements  function of get user by username

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,11 +1,22 @@
-import { UserInput } from "../schemas/userSchema";
-import { database } from "../database/db";
+import { UserInput } from "../schemas/userSchema.js";
+import { database } from "../database/db.js";
 import sqlBricks from "sql-bricks";
-
+import { User } from "../interfaces/user.interface.js";
 
 export const createUser = async (credentials: UserInput) => {
     const { text, values } = sqlBricks.insertInto('users', credentials)
         .toParams({ placeholder: '?' })
     const insertStatement = database.prepare(text)
     insertStatement.run(...values)
+}
+
+export const getUserByLogin = async (login: string) => {
+    const { text, values } = sqlBricks.select()
+        .from('users')
+        .where({ username: login })
+        .toParams({ placeholder: '?' })
+    const selectStatement = database.prepare(text)
+    const result = selectStatement.get(...values) as User
+    const user: User = { ...result }
+    return user
 }
