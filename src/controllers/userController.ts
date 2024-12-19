97b0a8c8390ee21f8cd39a87ac4a93f2d6602813
Gibson Cruz <gibson8fla@gmail.com>
2024-12-19 14:27:06 -0300

    feat: implements get users controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,14 +1,25 @@
-import { userValidation } from '../schemas/userSchema';
+import { UserInput, userValidation } from '../schemas/userSchema';
 import { FastifyReply, FastifyRequest } from "fastify";
 import * as userService from '../services/userService.js'
+import { User } from '../interfaces/user.interface';
 
 export const createUserController = async (req: FastifyRequest, reply: FastifyReply) => {
     try {
-        const data = userValidation.parse(req.body)
+        const data: UserInput = userValidation.parse(req.body)
         await userService.createUserService(data)
         reply.status(201).send({ message: `Usuário ${data.username} criado com sucesso!` })
     } catch (err) {
         console.error(err);
+        throw new Error(err)
+    }
+}
+
+export const getUsersController = async (_: FastifyRequest, reply: FastifyReply) => {
+    try {
+        const users: User[] = await userService.getUsersService()
+        users.length !== 0 ? reply.status(200).send(users) : reply.status(404).send({ message: 'Nenhum usuário encontrado' })
+    } catch (err) {
+        console.error(err)
         throw new Error(err)
     }
 }
