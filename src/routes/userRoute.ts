76d9ba4c73139a2265aa0817fdae8acb62bb5f0a
Gibson Cruz <gibson8fla@gmail.com>
2024-12-19 14:25:46 -0300

    feat: implements get users route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,3 +4,7 @@ import * as userController from '../controllers/userController.js'
 export const createUserRoute = async (server: FastifyInstance) => {
     server.post('/create', userController.createUserController)
 }
+
+export const getUsersRoute = async (server: FastifyInstance) => {
+    server.get('/users', userController.getUsersController)
+}
