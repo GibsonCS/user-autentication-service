521fa0f015910e1072a7a7e30832c3446860d46c
Gibson Cruz <gibson8fla@gmail.com>
2024-12-19 14:27:29 -0300

    feat: implements get users service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,8 +1,16 @@
 import { encryptPassword } from './../util/encrypt.js';
 import * as repository from '../repositories/userRepository.js'
-import { UserInput } from '../schemas/userSchema.js'
+import { User } from '../interfaces/user.interface.js';
+import { UserInput } from '../schemas/userSchema.js';
 
 export const createUserService = async (credentials: UserInput) => {
     credentials.password = await encryptPassword(credentials.password)
     await repository.createUser(credentials)
+}
+
+export const getUsersService = async () => {
+    const users: User[] = await repository.getUsers();
+    const usersOnlyUsernameAndEmail: User[] = []
+    users.forEach(user => usersOnlyUsernameAndEmail.push({ username: user.username, email: user.email }))
+    return usersOnlyUsernameAndEmail
 }
