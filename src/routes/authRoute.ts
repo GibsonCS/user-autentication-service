b03071c74d79e4412e816dbe5eab58da8aa40572
Gibson Cruz <gibson8fla@gmail.com>
2024-12-31 16:03:36 -0300

    feat: creat auth route

---
+++ src/routes/authRoute.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { jwtDecoded } from "../util/jwdDecode";
+
+
+export const authRoute = async (server: FastifyInstance) => {
+    server.get('/check-auth', (request: FastifyRequest, reply: FastifyReply) => {
+        const token = request.cookies.authToken
+        const decodedToke = jwtDecoded(token)
+        reply.send({ username: decodedToke.username, roles: [decodedToke.roles] })
+    })
+}
