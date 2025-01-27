d4e81b800065fef515567e303312abb9095fd71e
Gibson Cruz <gibson8fla@gmail.com>
2025-01-27 11:51:46 -0300

    feat: implements login test

---
+++ src/api.test.js
@@ -0,0 +1,41 @@
+import { describe, before, after, it } from 'node:test'
+import { strictEqual, deepStrictEqual } from 'node:assert'
+import { resolve } from 'node:path'
+import { UserService } from './services/UserService.js'
+
+const BASE_URL = 'http://localhost:3000/api'
+const userService = new UserService()
+
+describe('API Workflow', () => {
+    let server
+    before(async () => {
+        server = (await import('./server.js')).server
+        await server.ready()
+        console.log('Server is ready')
+    })
+    before(async () => {
+        const user = {
+            username: "gibson",
+            password: "123456",
+            email: "gibson@gibson.com"
+        }
+        await userService.create(user)
+    })
+
+    it('Sucessfuly user and passeword login', async () => {
+        const credentials = {
+            username: "gibson",
+            password: "123456",
+        }
+        const response = await fetch(`${BASE_URL}/auth/login`, {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(credentials),
+        })
+        const data = await response.json()
+        strictEqual(response.status, 200)
+        deepStrictEqual(data.message, 'Wellcome')
+    })
+
+    after(async () => await server.close())
+})
