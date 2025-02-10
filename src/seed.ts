d903d17f7fafabd6271986ad381a749e2fee603e
GibsonCS <gibson.cruzds@gmail.com>
2025-02-09 22:28:43 -0300

    refactor: changes function name in the src/seed.ts

--- src/seed.ts
+++ src/seed.ts
@@ -2,7 +2,7 @@ import { UserInput } from './modules/users/schemas/userSchema.js'
 import { UserService } from './modules/users/services/UserService.js'
 import { UserRepositoryFactory } from './shared/factories/UserRepositoryFactory.js'
 
-const userRepository = UserRepositoryFactory.createUserRepository("prod")
+const userRepository = UserRepositoryFactory.createUserRepository('prod')
 const userService = new UserService(userRepository)
 export const seed = async () => {
   const user: UserInput = {
@@ -10,5 +10,5 @@ export const seed = async () => {
     password: 'teste',
     email: 'teste@teste.com',
   }
-  await userService.create(user);
+  await userService.registerUser(user)
 }
