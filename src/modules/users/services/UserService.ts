1c2080377b5e0fda896a572aef446ebbc4feeecd
GibsonCS <gibson.cruzds@gmail.com>
2025-02-10 22:00:30 -0300

    refactor: src/modules/users/services/UserService.ts

--- src/modules/users/services/UserService.ts
+++ src/modules/users/services/UserService.ts
@@ -1,6 +1,10 @@
+import { Jwt } from 'jsonwebtoken'
 import { encryptPassword } from '../../../shared/util/encrypt.js'
 import { IUserRepository } from '../interfaces/IUserRepository.js'
 import { LoginInput, UserInput, UserOutput } from '../schemas/userSchema.js'
+import { generateJWT } from '../../../shared/util/jwtGenerate.js'
+import { User } from '../entity/User.js'
+import { comparePassword } from '../../../shared/util/comparePassword.js'
 
 export class UserService {
   private userRepository: IUserRepository
@@ -8,26 +12,36 @@ export class UserService {
     this.userRepository = userRepository
   }
 
-  create = async (user: UserInput): Promise<UserOutput> => {
+  registerUser = async (user: UserInput): Promise<UserOutput> => {
     user.password = await encryptPassword(user.password)
     const userCreated = await this.userRepository.create(user)
     return userCreated as UserOutput
   }
 
-  findAll = async () => {
+  getUsers = async () => {
     const users = await this.userRepository.findAll()
     const userOnlyEmailAndUsername: UserOutput[] = []
     users.forEach((user) =>
       userOnlyEmailAndUsername.push({
         username: user.username,
         email: user.email,
-      }),
+      })
     )
     return userOnlyEmailAndUsername
   }
 
-  handleLogin = async (credentials: LoginInput) => {
-    const user = this.userRepository.findByLogin(credentials.username)
-    console.log(user)
+  loginUser = async (credentials: LoginInput): Promise<string> => {
+    const user: User = await this.userRepository.findByLogin(credentials.username)
+    if (!user) {
+      return null
+    }
+    const isMatch = await comparePassword(credentials.password, user.password)
+    if (isMatch) {
+      const jwt = generateJWT({
+        role: user.role,
+      })
+      return jwt
+    }
+    return null
   }
 }
