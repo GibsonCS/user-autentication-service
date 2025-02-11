91622046142aaab42613152a67e0b7c8864087e6
GibsonCS <gibson.cruzds@gmail.com>
2025-02-10 21:54:14 -0300

    feat: add return type in function src/shared/util/comparePassword.ts

--- src/shared/util/comparePassword.ts
+++ src/shared/util/comparePassword.ts
@@ -1,5 +1,8 @@
 import bcrypt from 'bcrypt'
 
-export const comparePassword = async (sendedPassword: string, passwordFromDB: string) => {
+export const comparePassword = async (
+  sendedPassword: string,
+  passwordFromDB: string
+): Promise<boolean> => {
   return await bcrypt.compare(sendedPassword, passwordFromDB)
 }
