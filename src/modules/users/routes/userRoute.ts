881e819e405ff49227a020d0cefa2d1ea1eead7d
GibsonCS <gibson.cruzds@gmail.com>
2025-02-10 21:59:52 -0300

    refactor: method name in src/modules/users/routes/userRoute.ts

--- src/modules/users/routes/userRoute.ts
+++ src/modules/users/routes/userRoute.ts
@@ -3,7 +3,7 @@ import { UserController } from '../controllers/UserController.js'
 
 const userController = new UserController()
 export const userRoutes = async (server: FastifyInstance) => {
-  server.post('/users', userController.createUser)
-  server.post('/users/login', userController.login)
-  server.get('/user', userController.getAllUsersController)
+  server.post('/users', userController.handleUserRegister)
+  server.post('/users/login', userController.handleLogin)
+  server.get('/users', userController.handleGettingUsers)
 }
