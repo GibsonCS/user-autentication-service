5b364ff0bc860e4a9b1c121d0610fba56b28bdad
GibsonCS <gibson.cruzds@gmail.com>
2025-02-10 21:56:55 -0300

    chore: add user type in find by login function in src/modules/users/interfaces/IUserRepository.ts

--- src/modules/users/interfaces/IUserRepository.ts
+++ src/modules/users/interfaces/IUserRepository.ts
@@ -1,7 +1,8 @@
+import { User } from '../entity/User.js'
 import { UserInput, UserOutput } from '../schemas/userSchema.js'
 
 export interface IUserRepository {
   create(credentials: UserInput): Promise<UserOutput>
   findAll(): Promise<UserOutput[]>
-  findByLogin(login: string): Promise<any>
+  findByLogin(login: string): Promise<User>
 }
