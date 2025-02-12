b293d7a2b770961681f1d009c3abd58a74910ea3
GibsonCS <gibson.cruzds@gmail.com>
2025-02-12 13:52:15 -0300

    feat: add user repository mock in the src/shared/factories/UserRepositoryFactory.ts

--- src/shared/factories/UserRepositoryFactory.ts
+++ src/shared/factories/UserRepositoryFactory.ts
@@ -1,11 +1,13 @@
 import { database } from '../database/db.js'
 import { UserRepository } from '../../modules/users/repositories/UserRepository.js'
+import { UserRepositoryMock } from '../../modules/users/repositories/UserRepositoryMock.js'
 
 export class UserRepositoryFactory {
-  static createUserRepository = (type: 'prod' | 'test'): UserRepository => {
+  static createUserRepository = (type: 'prod' | 'test'): UserRepository | UserRepositoryMock => {
     if (type === 'prod') {
       return new UserRepository(database)
     } else {
+      return new UserRepositoryMock()
     }
   }
 }
