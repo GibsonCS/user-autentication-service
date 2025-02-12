6f3ba2c77068ca8a1f42828e440659bd6b8b7b92
GibsonCS <gibson.cruzds@gmail.com>
2025-02-12 13:51:27 -0300

    feat: register auth route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { userRoutes } from '../modules/users/routes/userRoute.js'
+import { authRoutes } from '../modules/auth/routes/auth.js'
 
 export const registerRoutes = (server: FastifyInstance) => {
   const apiPrefix = {
@@ -7,4 +8,5 @@ export const registerRoutes = (server: FastifyInstance) => {
   }
 
   server.register(userRoutes, apiPrefix)
+  server.register(authRoutes, apiPrefix)
 }
