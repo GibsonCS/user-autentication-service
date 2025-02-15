db1946be248f11fb52ac16612f1a5d240e6cb596
GibsonCS <gibson.cruzds@gmail.com>
2025-02-14 23:13:48 -0300

    refactor: authMiddleware.ts and verify if token is valid in the midlleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,13 +1,21 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import { jwtDecoded } from '../shared/util/jwtDecode.js'
+import { extractToken } from '../shared/util/extractToken.js'
 
 export const authMiddleware = (server: FastifyInstance) => {
+  const publicRoutes = ['/api/users/login', '/api/users']
+
   server.addHook('onRequest', (req: FastifyRequest, reply: FastifyReply, done) => {
-    if (req.url === '/api/users/login' || req.url === '/api/users') {
-      if (req.method === 'POST') {
-        return done()
-      }
-    }
-    const authToken = req.headers.cookie
-    authToken ? done() : reply.status(401).send({ message: 'Unauthorized' })
+    if (publicRoutes.includes(req.url) && req.method === 'POST') return done()
+
+    const token = extractToken(req.headers.cookie)
+
+    if (!token) return reply.status(401).send({ message: 'Unauthorized' })
+
+    const payload: any = jwtDecoded(token)
+
+    if (!payload) return reply.status(401).send({ message: 'Unauthorized' })
+
+    return done()
   })
 }
