b911d5b9f6c48c67afcf0a08589765915a9c7183
GibsonCS <gibson.cruzds@gmail.com>
2025-02-14 23:15:50 -0300

    refactor: src/modules/auth/controllers/Auth.ts

--- src/modules/auth/controllers/Auth.ts
+++ src/modules/auth/controllers/Auth.ts
@@ -1,14 +1,11 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { jwtDecoded } from '../../../shared/util/jwtDecode.js'
+import { extractToken } from '../../../shared/util/extractToken.js'
 
 export class AuthController {
   handleAuth = async (request: FastifyRequest, reply: FastifyReply) => {
-    const authToken = request.headers.cookie
-    if (authToken) {
-      const tokenWithoutPrefix = authToken.split(';')[0].replace('authToken=', '')
-      const payload: any = jwtDecoded(tokenWithoutPrefix)
-      return reply.send({ role: `${payload.role}` })
-    }
-    return reply.status(401)
+    const token = extractToken(request.headers.cookie)
+    const payload: any = jwtDecoded(token)
+    return reply.send({ role: `${payload.role}` })
   }
 }
