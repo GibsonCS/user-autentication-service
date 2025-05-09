b8dd9ad209a2ffcc465e1eab6c32a0e1713883bc
GibsonCS <gibson.cruzds@gmail.com>
2025-05-09 16:34:48 -0300

    feat: add authSwaggerSchema in /auth router

--- src/modules/auth/routes/auth.ts
+++ src/modules/auth/routes/auth.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { AuthController } from '../controllers/Auth.js'
+import { authSwaggerSchema } from '../../../documentation/schemas/authSwaggerSchema.js'
 
 const controlller = new AuthController()
 export const authRoutes = async (server: FastifyInstance) => {
-  server.get('/auth', controlller.handleAuth)
+  server.get('/auth', authSwaggerSchema, controlller.handleAuth)
 }
