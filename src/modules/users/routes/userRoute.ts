affde28a16fe308196340dc919ff4b10d0d19143
GibsonCS <gibson.cruzds@gmail.com>
2025-05-09 16:36:13 -0300

    feat: adds swaggerUserCreateSchema swaggerLoginSchema swaggerGetUserSchema in users route

--- src/modules/users/routes/userRoute.ts
+++ src/modules/users/routes/userRoute.ts
@@ -1,9 +1,14 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import { FastifyInstance } from 'fastify'
 import { UserController } from '../controllers/UserController.js'
+import {
+  swaggerUserCreateSchema,
+  swaggerLoginSchema,
+  swaggerGetUserSchema,
+} from '../../../documentation/schemas/userSwaggerSchema.js'
 
 const userController = new UserController()
 export const userRoutes = async (server: FastifyInstance) => {
-  server.post('/users', userController.handleUserRegister)
-  server.post('/users/login', userController.handleLogin)
-  server.get('/users', userController.handleGettingUsers)
+  server.post('/users', swaggerUserCreateSchema, userController.handleUserRegister)
+  server.post('/users/login', swaggerLoginSchema, userController.handleLogin)
+  server.get('/users', swaggerGetUserSchema, userController.handleGettingUsers)
 }
